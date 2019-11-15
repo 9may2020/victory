cadbb201a5ab285961400511e32bc26171cb06f2
9may2020 <av.davydkov@gmail.com>
2019-11-15 10:48:08 +0300

    Updates

--- precache-manifest.760ac196989d49a7c8c094c52d851cd6.js
+++ precache-manifest.760ac196989d49a7c8c094c52d851cd6.js
@@ -1,166 +1,166 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4fd95660c7f35d80f89c0f9f9077d82",
-    "url": "/index.html"
+    "revision": "25f8f050b5651f4a74b74c60b3a6f09b",
+    "url": "/victory/index.html"
   },
   {
     "revision": "441b8b57039bec65bd9c",
-    "url": "/static/css/main.a8a2c937.chunk.css"
+    "url": "/victory/static/css/main.a8a2c937.chunk.css"
   },
   {
     "revision": "fb47dfdc40151c64393f",
-    "url": "/static/js/2.e6fb8f57.chunk.js"
+    "url": "/victory/static/js/2.e6fb8f57.chunk.js"
   },
   {
     "revision": "441b8b57039bec65bd9c",
-    "url": "/static/js/main.4c6ae4db.chunk.js"
+    "url": "/victory/static/js/main.4c6ae4db.chunk.js"
   },
   {
-    "revision": "be68dc4b89bcd57af312",
-    "url": "/static/js/runtime-main.f915f1c6.js"
+    "revision": "1243532e0caeca6ec7ec",
+    "url": "/victory/static/js/runtime-main.a4c0497b.js"
   },
   {
     "revision": "59da5c6f19d3b2fcd4d9cda0f1a2665a",
-    "url": "/static/media/azorizdestihie.59da5c6f.jpg"
+    "url": "/victory/static/media/azorizdestihie.59da5c6f.jpg"
   },
   {
     "revision": "7408c920277ec1560ebc3b2b1452ea1b",
-    "url": "/static/media/azorizdestihie.7408c920.jpg"
+    "url": "/victory/static/media/azorizdestihie.7408c920.jpg"
   },
   {
     "revision": "e105372988ab99a300751397435c4012",
-    "url": "/static/media/bagration.e1053729.jpg"
+    "url": "/victory/static/media/bagration.e1053729.jpg"
   },
   {
     "revision": "00cf464658b5ca41610bec4051c250b3",
-    "url": "/static/media/bagration2.00cf4646.jpg"
+    "url": "/victory/static/media/bagration2.00cf4646.jpg"
   },
   {
     "revision": "05fb3000b4d6561d549c451431108bce",
-    "url": "/static/media/bagration3.05fb3000.jpg"
+    "url": "/victory/static/media/bagration3.05fb3000.jpg"
   },
   {
     "revision": "c320959f53912d5e533bd5b07e4f913c",
-    "url": "/static/media/bagration4.c320959f.jpg"
+    "url": "/victory/static/media/bagration4.c320959f.jpg"
   },
   {
     "revision": "99cc9a2c1b4f2f8c244e9f7868342c4d",
-    "url": "/static/media/berlin.99cc9a2c.jpg"
+    "url": "/victory/static/media/berlin.99cc9a2c.jpg"
   },
   {
     "revision": "306539b3465be8f2e40578b292afc319",
-    "url": "/static/media/book.306539b3.jpg"
+    "url": "/victory/static/media/book.306539b3.jpg"
   },
   {
     "revision": "33ea7bf8681334d2d7d022cf267a60f6",
-    "url": "/static/media/brest.33ea7bf8.jpg"
+    "url": "/victory/static/media/brest.33ea7bf8.jpg"
   },
   {
     "revision": "6297b9983dc49307fcd22ed1c3edd983",
-    "url": "/static/media/elna.6297b998.jpg"
+    "url": "/victory/static/media/elna.6297b998.jpg"
   },
   {
     "revision": "344f0b0cf7560dd5ba96c4ca7facaa39",
-    "url": "/static/media/event.344f0b0c.jpg"
+    "url": "/victory/static/media/event.344f0b0c.jpg"
   },
   {
     "revision": "e7c9f7510cfff952b63a6e02acd43ea9",
-    "url": "/static/media/home-bg.e7c9f751.jpg"
+    "url": "/victory/static/media/home-bg.e7c9f751.jpg"
   },
   {
     "revision": "3cd8bec245f2a921285fe006ee5b84c9",
-    "url": "/static/media/kavkaz.3cd8bec2.jpg"
+    "url": "/victory/static/media/kavkaz.3cd8bec2.jpg"
   },
   {
     "revision": "1924f5d081078fa2f12a7f6c886fd38b",
-    "url": "/static/media/kiev.1924f5d0.jpg"
+    "url": "/victory/static/media/kiev.1924f5d0.jpg"
   },
   {
     "revision": "1fe45d5cae415d1ebdeb3303630ee485",
-    "url": "/static/media/koncert.1fe45d5c.jpg"
+    "url": "/victory/static/media/koncert.1fe45d5c.jpg"
   },
   {
     "revision": "5e9f1f4cacb46e3f5c9643080fc9f1a4",
-    "url": "/static/media/krym.5e9f1f4c.jpg"
+    "url": "/victory/static/media/krym.5e9f1f4c.jpg"
   },
   {
     "revision": "bca5ec4a02bbf81358f0c98c6e8e201e",
-    "url": "/static/media/lenta.bca5ec4a.jpg"
+    "url": "/victory/static/media/lenta.bca5ec4a.jpg"
   },
   {
     "revision": "03ae7c0c593d7a728151127f6638ec0a",
-    "url": "/static/media/logo.03ae7c0c.jpg"
+    "url": "/victory/static/media/logo.03ae7c0c.jpg"
   },
   {
     "revision": "1f1695b0ed656ea2a7d38189a90aee14",
-    "url": "/static/media/moscow.1f1695b0.png"
+    "url": "/victory/static/media/moscow.1f1695b0.png"
   },
   {
     "revision": "4d78af9216dca9a6ed8378c1c93af7e5",
-    "url": "/static/media/music.4d78af92.jpg"
+    "url": "/victory/static/media/music.4d78af92.jpg"
   },
   {
     "revision": "c83a4b6ce479c3697bc62e71253df79b",
-    "url": "/static/media/navoine.c83a4b6c.jpg"
+    "url": "/victory/static/media/navoine.c83a4b6c.jpg"
   },
   {
     "revision": "f49caca6bf6d7985fa7658daee5b2cb1",
-    "url": "/static/media/oficeru.f49caca6.jpg"
+    "url": "/victory/static/media/oficeru.f49caca6.jpg"
   },
   {
     "revision": "0e30dd7b8af3f1157fac1972ef35b9b6",
-    "url": "/static/media/rostov.0e30dd7b.jpg"
+    "url": "/victory/static/media/rostov.0e30dd7b.jpg"
   },
   {
     "revision": "20a78a3332aa48d63445b738d145ce4b",
-    "url": "/static/media/rzhev.20a78a33.jpg"
+    "url": "/victory/static/media/rzhev.20a78a33.jpg"
   },
   {
     "revision": "fa50756b40fc52d22bc593090697470d",
-    "url": "/static/media/silezia.fa50756b.jpg"
+    "url": "/victory/static/media/silezia.fa50756b.jpg"
   },
   {
     "revision": "6fc3df52979bfebfbb886c91bb66df33",
-    "url": "/static/media/simonov.6fc3df52.jpg"
+    "url": "/victory/static/media/simonov.6fc3df52.jpg"
   },
   {
     "revision": "f424583d7f59bc48502c06251c7ab427",
-    "url": "/static/media/stalingrad.f424583d.jpg"
+    "url": "/victory/static/media/stalingrad.f424583d.jpg"
   },
   {
     "revision": "f1f27b37f1a764aa919757deb20fb214",
-    "url": "/static/media/stariki.f1f27b37.jpg"
+    "url": "/victory/static/media/stariki.f1f27b37.jpg"
   },
   {
     "revision": "e306b2506b2763d78c85c9de71e13abf",
-    "url": "/static/media/sydba.e306b250.jpg"
+    "url": "/victory/static/media/sydba.e306b250.jpg"
   },
   {
     "revision": "a6324a843c12c83dda20dd9270713af1",
-    "url": "/static/media/syvorov.a6324a84.jpg"
+    "url": "/victory/static/media/syvorov.a6324a84.jpg"
   },
   {
     "revision": "a0e6faf54e60209a3c94d6ba692fa99d",
-    "url": "/static/media/ukraina.a0e6faf5.jpg"
+    "url": "/victory/static/media/ukraina.a0e6faf5.jpg"
   },
   {
     "revision": "47dfb815db38cd818fcef98ba2d145f7",
-    "url": "/static/media/vena.47dfb815.jpg"
+    "url": "/victory/static/media/vena.47dfb815.jpg"
   },
   {
     "revision": "972b92523cc12987a6430a66a7e1889d",
-    "url": "/static/media/vesna.972b9252.jpg"
+    "url": "/victory/static/media/vesna.972b9252.jpg"
   },
   {
     "revision": "43751929a7f80f41e8a7730a1e45d365",
-    "url": "/static/media/video.43751929.jpg"
+    "url": "/victory/static/media/video.43751929.jpg"
   },
   {
     "revision": "71a7a0cfd2adfb30ea65724a90d2381f",
-    "url": "/static/media/zaRodiny.71a7a0cf.jpg"
+    "url": "/victory/static/media/zaRodiny.71a7a0cf.jpg"
   },
   {
     "revision": "a1d017d5a0db379424f33c8095efcc1a",
-    "url": "/static/media/zhuravli.a1d017d5.jpg"
+    "url": "/victory/static/media/zhuravli.a1d017d5.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1420a40718e73d8a67408fed9ee2dd1a.js"
+  "/victory/precache-manifest.760ac196989d49a7c8c094c52d851cd6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/victory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
